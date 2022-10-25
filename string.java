6bca845675ea6931efc2df4644f211c3396a5c42
MAHAPRABU S <115365595+mahaprabusivasamy@users.noreply.github.com>
2022-10-25 23:01:01 +0530

    Create string.java
    Scan the name and print the scanned name and length of the name

---
+++ string.java
@@ -0,0 +1,19 @@
+
+/*Scan the name and print the name and how many letters in the name*/
+
+
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    Scanner s=new Scanner(System.in);
+	    String name;
+	    name=s.nextLine();
+	    System.out.println(name);
+	     System.out.println("length of the string: "+name.length());
+}
+}
+//input : robin
+// output : robin
+///         length of thr string: 5
