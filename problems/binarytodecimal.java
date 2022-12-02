ac14c2342bc2381065ead89e7f8ec538d9e41a6a
MAHAPRABU S <115365595+mahaprabusivasamy@users.noreply.github.com>
2022-12-02 23:13:05 +0530

    convert binary to decimal value in java

---
+++ problems/binarytodecimal.java
@@ -0,0 +1,15 @@
+// You are using Java
+import java.util.*;
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner ss=new Scanner(System.in);
+      String ch=ss.nextLine();
+      System.out.println(Integer.parseInt(ch,2));
+      
+    }
+}
+/*
+INPUT: 1101
+OUTPUT: 13*/
